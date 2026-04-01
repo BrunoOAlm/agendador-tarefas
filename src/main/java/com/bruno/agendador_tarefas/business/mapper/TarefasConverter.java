0f02f869dbd38a715d0932fed46239f19333e167
Bruno <brunoalm75@gmail.com>
2026-04-01 08:56:49 -0300

    adicionado metodo push

---
+++ src/main/java/com/bruno/agendador_tarefas/business/mapper/TarefasConverter.java
@@ -0,0 +1,14 @@
+package com.bruno.agendador_tarefas.business.mapper;
+
+import com.bruno.agendador_tarefas.business.dto.TarefasDTO;
+import com.bruno.agendador_tarefas.infrastructure.entity.TarefasEntity;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface TarefasConverter {
+
+    TarefasEntity paraTarefaEntity(TarefasDTO dto);
+
+    TarefasDTO paraTarefaDTO(TarefasEntity entity);
+
+}
---
+++ src/main/java/com/bruno/agendador_tarefas/infrastructure/enums/StatusNotificacaoEnum.java
@@ -0,0 +1,7 @@
+package com.bruno.agendador_tarefas.infrastructure.enums;
+
+import org.springframework.security.core.parameters.P;
+
+public enum StatusNotificacaoEnum {
+        PENDENTE,NOTIFICADO,CANCELADO
+}
